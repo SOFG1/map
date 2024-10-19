76ea56d8164af4deea73c7c450f1c37c6504ee20
Edaurd Kosyan <edkosyan2000@gmail.com>
2024-10-19 13:22:38 +0400

    demo v1

--- popup.js
+++ popup.js
@@ -1,5 +1,63 @@
-const imageSrc =
-  "https://media.istockphoto.com/id/511061090/photo/business-office-building-in-london-england.jpg?s=612x612&w=0&k=20&c=nYAn4JKoCqO1hMTjZiND1PAIWoABuy1BwH1MhaEoG6w=";
+function generatePopupHtml() {
+  return `
+      <img src="./images/1.jpg" alt="" class="popup__img" />
+      <div class="popup__box">
+        <div class="popup__content">
+          <p class="popup__title">Mafia Kyiv Game</p>
+          <p class="popup__city">Kyiv, Ukraine</p>
+          <p class="popup__link">
+            <img src="./images/icon4.svg" alt="" class="popup__icon" /> ресторан
+            "Причал", Набережне Шосе, 11
+          </p>
+          <p class="popup__link">
+            <img src="./images/icon3.svg" alt="" class="popup__icon" /> +38 050
+            761 96 98
+          </p>
+          <a
+            href="https://t.me/MafiaKyivGame"
+            target="_blank"
+            class="popup__link popup__link_blue"
+          >
+            <img src="./images/icon2.svg" alt="" class="popup__icon" />
+            t.me/MafiaKyivGame
+          </a>
+          <a
+            href="https://instagram.com/Man_of_Mafia"
+            target="_blank"
+            class="popup__link popup__link_blue"
+          >
+            <img src="./images/icon1.svg" alt="" class="popup__icon" />
+            Instagram.com/Man_of_Mafia
+          </a>
+        </div>
+        <img src="./images/logo.png" alt="" class="popup__logo" />
+      </div>
+      <div class="popup__text">
+        Створюємо гарний настрій вже 13 років! Граємо кожні Ср, Пт та Нд в
+        різних ресторанах в центрі міста. Завжди раді новим обличчям!
+      </div>
+      <div class="popup__slider">
+        <button class="popup__slider-btn popup__slider-btn_left">
+          <img
+            src="./images/arrow-left.svg"
+            alt=""
+            class="popup__slider-arrow"
+          />
+        </button>
+        <button class="popup__slider-btn popup__slider-btn_right">
+          <img
+            src="./images/arrow-right.svg"
+            alt=""
+            class="popup__slider-arrow"
+          />
+        </button>
+        <img src="./images/1.jpg" alt="" class="popup__slider-img" />
+        <img src="./images/2.jpg" alt="" class="popup__slider-img" />
+        <img src="./images/1.jpg" alt="" class="popup__slider-img" />
+      </div>
+      <img src="./images/stripes.svg" class="popup__stripes" />
+  `;
+}
 
 // When a click event occurs on a feature in the unclustered-point layer, display a popup
 map.on("click", "unclustered-point", (e) => {
@@ -13,17 +71,6 @@ map.on("click", "unclustered-point", (e) => {
 
   new mapboxgl.Popup()
     .setLngLat(coordinates)
-    .setHTML(
-      `<div>
-      <h2>Mafia club #1</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      <img
-        height="200"
-        width="200"
-        src="${imageSrc}"
-        alt=""
-      />
-      </div>`
-    )
+    .setHTML(generatePopupHtml())
     .addTo(map);
 });
--- setup.js
+++ setup.js
@@ -1,12 +1,19 @@
 mapboxgl.accessToken =
   "pk.eyJ1Ijoib25lcGVhY2UiLCJhIjoiY2t1eHUxMmpiMGhxaTJucXFjd2k5ZjE2OSJ9.MwH_NiqfqOlyMU3j50Z1dw";
 
+const style = "mapbox://styles/mapbox/streets-v9";
+
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoib25lcGVhY2UiLCJhIjoiY2t1eHUxMmpiMGhxaTJucXFjd2k5ZjE2OSJ9.MwH_NiqfqOlyMU3j50Z1dw";
 const map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/mapbox/streets-v9",
-  //   projection: "globe", // Display the map as a globe, since satellite-v9 defaults to Mercator
-  center: [30.45, 50.45], // Initial map center [longitude, latitude]
-  zoom: 10, //
+  container: "map", // container ID
+  center: [30.44, 50.46], // starting position [lng, lat]
+  zoom: 2.8, // starting zoom
+  pitch: 30, // starting pitch
+});
+
+map.on("load", () => {
+  // Add a 3D building layer with fill-extrusion properties
 });
 
 map.addControl(new mapboxgl.NavigationControl());
