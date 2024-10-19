8ef75ad3b212b36f810af16aa84cc9ae4ba56156
Edaurd Kosyan <edkosyan2000@gmail.com>
2024-10-19 16:41:39 +0400

    change iamge path

--- load.js
+++ load.js
@@ -1,9 +1,9 @@
 function waitForLoad() {
   return new Promise((res) =>
     map.on("load", async () => {
-      map.loadImage("/images/pin1.png", (err, img) => {
+      map.loadImage("/pin1.png", (err, img) => {
         map.addImage("pin1", img);
-        map.loadImage("/images/pin2.png", (err, img) => {
+        map.loadImage("/pin2.png", (err, img) => {
           map.addImage("pin2", img);
           res();
         });
