4419d6ced5571675191eaaf58d404c36e9109efa
Edaurd Kosyan <edkosyan2000@gmail.com>
2024-11-04 16:11:38 +0400

    show all icons

--- points.js
+++ points.js
@@ -33,6 +33,6 @@ map.addLayer({
     // "icon-image": ["get", "pin"],
     "icon-image": "pin2",
     "icon-anchor": "bottom",
-    // "icon-allow-overlap": true,
+    "icon-allow-overlap": true,
   },
 });
