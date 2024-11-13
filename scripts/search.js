b2474ea14bb4fcb0e60887416b18761f825c64dd
Edaurd Kosyan <edkosyan2000@gmail.com>
2024-11-13 18:18:32 +0400

    add autocomplete

---
+++ scripts/search.js
@@ -0,0 +1,11 @@
+const config = {
+  placeHolder: "Search for clubs",
+  data: {
+    src: ["Test", "ewfewwe", "eeeeeeeeeee"],
+  },
+  resultItem: {
+    highlight: true,
+  },
+};
+
+const autoCompleteJS = new autoComplete(config);
